b1087fce7f34ebe6dab5eedad002f41bc168b91e
Leonardo Azenha <azenha.leonardo@gmail.com>
2021-11-17 15:49:35 -0300

    atualização da aula

---
+++ pi2_2021_2_api_produtos/rotas/usuario_rota.js
@@ -0,0 +1,13 @@
+//Rota: /usuarios (localhost:3000/usuarios)
+
+const express = require('express');
+const rota = express.Router();
+
+const usuarioController = require('../controller/usuario_controller');
+
+rota.get('/', usuarioController.listar);
+rota.post('/', usuarioController.inserir);
+rota.get('/busca', usuarioController.buscarPorUsername);
+rota.get('/:id', usuarioController.buscarPorId);
+
+module.exports = rota;
